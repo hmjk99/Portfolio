535e290ebb060997646349646e674e80e589bf46
Moonjoo Kim <moonjookim@MacBook-Air.fios-router.home>
2023-03-02 01:51:57 -0500

    made some final changes

--- app.js
+++ app.js
@@ -18,6 +18,43 @@ $(()=>{
         $navMenu.removeClass('show')
     })
 
+    // ---------------displaying page-----------------//
+    $('#nav-about').on('click', ()=>{
+        $('#Home').css('display', 'none')
+        $('#About').css('display', 'block')
+        $('#Skills').css('display', 'none')
+        $('#Projects').css('display', 'none')
+        $('#Contact').css('display', 'none')
+    })
+    $('#nav-skills').on('click', ()=>{
+        $('#Home').css('display', 'none')
+        $('#About').css('display', 'none')
+        $('#Skills').css('display', 'block')
+        $('#Projects').css('display', 'none')
+        $('#Contact').css('display', 'none')
+    })
+    $('#nav-project').on('click', ()=>{
+        $('#Home').css('display', 'none')
+        $('#About').css('display', 'none')
+        $('#Skills').css('display', 'none')
+        $('#Projects').css('display', 'block')
+        $('#Contact').css('display', 'none')
+    })
+    $('#nav-contact').on('click', ()=>{
+        $('#Home').css('display', 'none')
+        $('#About').css('display', 'none')
+        $('#Skills').css('display', 'none')
+        $('#Projects').css('display', 'none')
+        $('#Contact').css('display', 'flex')
+    })
+    $('#nav-home').on('click', ()=>{
+        $('#Home').css('display', 'flex')
+        $('#About').css('display', 'none')
+        $('#Skills').css('display', 'none')
+        $('#Projects').css('display', 'none')
+        $('#Contact').css('display', 'none')
+    })
+
     // ---------skills toggle--------//
     const $skillsFrontHeader = $('.front')
     const $skillsBackHeader = $('.back')
@@ -27,10 +64,10 @@ $(()=>{
         $skillsFront.slideToggle(1000)
     })
     $skillsBackHeader.on('click', ()=>{
-        $skillsBack.slideToggle(1500)
+        $skillsBack.slideToggle(1000)
     })
 
-    // ---------carousel----------//
+    // ------------------------carousel------------------------//
     let currentImgIndex = 0
     const $numOfImages = $('#imgs-carousel').children().length - 1
     const $content = $('#imgs-carousel').children()
@@ -72,40 +109,5 @@ $(()=>{
         $('#main-img').toggleClass('dark-border')
     })
 
-    // displaying page
-    $('#nav-about').on('click', ()=>{
-        $('#Home').css('display', 'none')
-        $('#About').css('display', 'block')
-        $('#Skills').css('display', 'none')
-        $('#Projects').css('display', 'none')
-        $('#Contact').css('display', 'none')
-    })
-    $('#nav-skills').on('click', ()=>{
-        $('#Home').css('display', 'none')
-        $('#About').css('display', 'none')
-        $('#Skills').css('display', 'block')
-        $('#Projects').css('display', 'none')
-        $('#Contact').css('display', 'none')
-    })
-    $('#nav-project').on('click', ()=>{
-        $('#Home').css('display', 'none')
-        $('#About').css('display', 'none')
-        $('#Skills').css('display', 'none')
-        $('#Projects').css('display', 'block')
-        $('#Contact').css('display', 'none')
-    })
-    $('#nav-contact').on('click', ()=>{
-        $('#Home').css('display', 'none')
-        $('#About').css('display', 'none')
-        $('#Skills').css('display', 'none')
-        $('#Projects').css('display', 'none')
-        $('#Contact').css('display', 'flex')
-    })
-    $('#nav-home').on('click', ()=>{
-        $('#Home').css('display', 'flex')
-        $('#About').css('display', 'none')
-        $('#Skills').css('display', 'none')
-        $('#Projects').css('display', 'none')
-        $('#Contact').css('display', 'none')
-    })
+    
 })
